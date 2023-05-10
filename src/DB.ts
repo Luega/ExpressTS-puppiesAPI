d3d42e9192706f5bc74144d7e32e3f1db77b7cea
Luega <martinelliluca90@gmail.com>
2023-05-10 14:03:30 +0200

    API:
    created GET /api/puppies,
    created GET /api/puppies/:id,
    created POST /api/puppies,
    created PUT /api/puppies/:id,
    ceated DELETE /api/puppies/:id.
    
    DB:
    
    created fake DB,
    created functions to manage DB:
    getAllPuppies,
    getPuppy,
    createPuppy,
    updatePuppy,
    deletePuppy.

---
+++ src/DB.ts
@@ -0,0 +1,95 @@
+const { v4: uuidv4 } = require("uuid");
+
+export type Puppy = {
+  id?: string;
+  breed: string;
+  name: string;
+  birthDate: string;
+};
+
+type UpdateReq = {
+  breed?: string;
+  name?: string;
+  birthDate?: string;
+};
+
+let DB: Puppy[] = [
+  {
+    id: uuidv4(),
+    breed: "Maltese",
+    name: "Carlo",
+    birthDate: "01-12-2021",
+  },
+  {
+    id: uuidv4(),
+    breed: "Pastore tedesco",
+    name: "Gianni",
+    birthDate: "05-22-2022",
+  },
+  {
+    id: uuidv4(),
+    breed: "Labrador",
+    name: "Guglielmo",
+    birthDate: "06-21-2019",
+  },
+  {
+    id: uuidv4(),
+    breed: "Golden retriever",
+    name: "Roberto",
+    birthDate: "08-02-2018",
+  },
+  {
+    id: uuidv4(),
+    breed: "Carlino",
+    name: "Ugo",
+    birthDate: "11-22-2011",
+  },
+];
+
+export const getAllPuppies = () => {
+  return DB;
+};
+
+export const getPuppy = (reqPuppyId: string) => {
+  const puppy = DB.find((puppy) => puppy.id === reqPuppyId!);
+  return puppy;
+};
+
+export const createPuppy = (puppy: Puppy) => {
+  const newPuppy: Puppy = {
+    id: uuidv4(),
+    breed: puppy.breed,
+    name: puppy.name,
+    birthDate: puppy.birthDate,
+  };
+
+  DB.push(newPuppy);
+
+  return newPuppy;
+};
+
+export const updatePuppy = (updateReq: UpdateReq, puppy: Puppy) => {
+  const updatedPuppy: Puppy = {
+    id: puppy.id,
+    breed: updateReq.breed ? updateReq.breed : puppy.breed,
+    name: updateReq.name ? updateReq.name : puppy.name,
+    birthDate: updateReq.birthDate ? updateReq.birthDate : puppy.birthDate,
+  };
+
+  const index: number = DB.findIndex((e) => e.id === puppy.id);
+  DB.splice(index, 1, updatedPuppy);
+
+  return getPuppy(updatedPuppy.id!);
+};
+
+export const deletePuppy = (puppyId: string) => {
+  const index: number = DB.findIndex((puppy) => puppy.id === puppyId);
+  if (index === -1) {
+    return `No puppies found with id: ${puppyId}`;
+  }
+  DB.splice(index, 1);
+
+  return `Puppies with id:${puppyId} deleted`;
+};
+
+export default DB;
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,63 @@
 import express, { Express, Request, Response } from "express";
 
-const app: Express = express();
+import {
+  Puppy,
+  getAllPuppies,
+  getPuppy,
+  deletePuppy,
+  createPuppy,
+  updatePuppy,
+} from "./DB";
 
-app.get("/api/test", (_req: Request, res: Response) => {
-  return res.status(200).json({ test: "is working as it should" });
+const app: Express = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/api/puppies", (_req: Request, res: Response) => {
+  const puppies = getAllPuppies();
+  if (puppies) {
+    return res.status(200).json(puppies);
+  }
+  return res.status(200).json("No puppies found");
+});
+
+app.get("/api/puppies/:id", (req: Request, res: Response) => {
+  const reqPuppyId = req.params.id;
+  const puppy = getPuppy(reqPuppyId!);
+  if (puppy) {
+    return res.status(200).json(puppy);
+  }
+
+  return res.status(200).json("Puppy not found");
+});
+
+app.post("/api/puppies", (req: Request, res: Response) => {
+  if (req.body.breed && req.body.name && req.body.birthDate) {
+    const reqNewPuppy: Puppy = req.body;
+    const createdPuppy = createPuppy(reqNewPuppy);
+    return res.status(201).json(createdPuppy);
+  }
+
+  return res
+    .status(400)
+    .json("To creat a puppy we need to know breed, name, birthDate.");
+});
+
+app.put("/api/puppies/:id", (req: Request, res: Response) => {
+  const reqPuppyId = req.params.id;
+  const puppy = getPuppy(reqPuppyId!);
+  if (puppy) {
+    const updatedPuppy = updatePuppy(req.body, puppy);
+    return res.status(201).json(updatedPuppy);
+  }
+
+  return res.status(400).json("Puppy not found");
+});
+
+app.delete("/api/puppies/:id", (req: Request, res: Response) => {
+  const reqPuppyId = req.params.id;
+  const deleted: string = deletePuppy(reqPuppyId!);
+  return res.status(200).json(deleted);
 });
 
 export default app;
